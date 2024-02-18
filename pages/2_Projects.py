8a535e96d67d6441fff6c7d698afb920828f6e9b
金鑫 <xin.jin02@hlifetech.com>
2024-02-18 17:48:02 +0800

    move some files

--- pages/2_Projects.py
+++ pages/2_Projects.py
@@ -3,9 +3,9 @@ import streamlit as st
 
 
 st.header('售前项目')
-df_sq = pd.read_excel('区域数据源项目汇总.xlsx')
+df_sq = pd.read_excel('./data/xmhz.xlsx')
 st.write(df_sq)
 
 st.header('正式项目')
-df_zs = pd.read_excel('区域数据源项目汇总.xlsx', sheet_name='正式项目')
+df_zs = pd.read_excel('./data/xmhz.xlsx', sheet_name='正式项目')
 st.write(df_zs)
